795f41d0e69b42946e0879463e127a7fa824f5d1
user15544 <ryokofuruta@gmail.com>
2026-09-24 00:12:00 +0900

    kadai_020をアップロード

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-  const text = document.getElementById('text');
   text.textContent = 'ボタンをクリックしました';
   console.log(text);
 })
